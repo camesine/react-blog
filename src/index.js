e320e4efd2e730f03e15d87ef22e2c1203a2a975
Hector <hector.riquelme1169@gmail.com>
2018-01-16 21:45:45 -0300

    store redux integration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import ReduxPromise from 'redux-promise'
 
+import reducers from './reducers'
 import PostIndex from './components/post_index'
 
 ReactDOM.render(
-
-  <BrowserRouter>
-    <div>
-      <Route path="/" component={PostIndex} />
-    </div>
-  </BrowserRouter>
-  
-, document.getElementById('root'));
+  <Provider store={applyMiddleware(ReduxPromise)(createStore)(reducers)}>
+    <BrowserRouter>
+        <div>
+          <Route path="/" component={PostIndex} />
+        </div>
+      </BrowserRouter>
+  </Provider>
+, document.getElementById('root'))
